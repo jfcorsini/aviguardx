e3e674cebcc452ed832ceede07476fbe177dc395
João Fernando Corsini <jf.corsini@gmail.com>
2021-04-26 20:46:43 +0300

    Process data for 25 seconds

--- gnuradio/main.py
+++ gnuradio/main.py
@@ -3,10 +3,10 @@ import subprocess
 
 def read_from_antennas(name):
     '''
-    Runs read_data.py and waits 20 seconds before closing.
+    Runs read_data.py and waits 25 seconds before closing.
     This is done like this because the computer where we are using only
     has GNURadio installed in the python3.6.
     '''
-    subprocess.run(["timeout", "20", "python3.6",
+    subprocess.run(["timeout", "25", "python3.6",
                     "gnuradio/read_data.py", name])
     return name
