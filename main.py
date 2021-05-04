298198e19e58ffe5e03a7e4e0011ac12a7404a4d
João Fernando Corsini <jf.corsini@gmail.com>
2021-05-04 18:37:37 +0300

    Update status & create entries when processing

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from FRCNN_predict.predict import run_prediction
 from gnuradio.main import read_from_antennas
 from processing.main import run_processing
 from visualization.main import visualize
+import boto3
+import requests
+import datetime
 import sys
 import time
+
+SECRET = 'SECRET-HERE'
 
 
 def main():
@@ -13,10 +18,14 @@ def main():
         while(True):
             timestamp = str(int(time.time() * 1000))
             print('Starting to process filename=' + timestamp)
+            update_status('READING')
             read_from_antennas(timestamp)
+            update_status('TRACKING')
             run_processing(timestamp)
+            update_status('IDENTIFYING')
             coordinates = run_prediction(timestamp)
             visualize(coordinates, timestamp)
+            upload_entry(timestamp)
         return
 
     output_name = str(int(time.time() * 1000))
@@ -39,6 +48,55 @@ def main():
         run_processing(output_name)
         coordinates = run_prediction(output_name)
         visualize(coordinates, output_name)
+
+
+def update_status(status):
+    data = {
+        "status": status,
+        "secret": SECRET
+    }
+    url = "http://aviguardx.vercel.app/api/status"
+    response = requests.patch(url, data)
+    return
+
+
+def upload_file(file_name, bucket):
+    bucket = 'aviguardx'
+    file_path = os.path.join(os.getcwd(), "results", file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(
+            file_path,
+            bucket,
+            file_name,
+            ExtraArgs={'ACL': 'public-read'})
+        print('Response upload file', response)
+        return "https://aviguardx.s3.eu-north-1.amazonaws.com/{0}".format(file_name)
+    except e:
+        print('Error to upload file', e)
+        return ""
+
+
+def upload_entry(name):
+    simple_tracked_url = upload_file(name + '.jpeg')
+    tracked_url = upload_file(name + '_labeled.jpeg')
+    predicted_url = upload_file(name + '_predicted.jpeg')
+    map_url = upload_file(name + '_map.jpeg')
+    data = {
+        "map_url": map_url,
+        "tracked_url": tracked_url,
+        "predicted_url": predicted_url,
+        "simple_tracked_url": simple_tracked_url,
+        "name": name,
+        "recorded_at": datetime.datetime.now().isoformat(),
+        "jsonData": "{}",
+        "secret": SECRET
+    }
+    url = "http://aviguardx.vercel.app/api/entries"
+    response = requests.post(url, data)
+    return
 
 
 if __name__ == '__main__':
